b1fbb0bdc5345045d516170fa0a572f56741e3ce
Sven Bohm <bohms@hey.com>
2022-01-27 18:20:18 -0500

    upgraded dependencies

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Multipart.MixProject do
 
   defp deps do
     [
-      {:mime, "~> 1.2"},
+      {:mime, "~> 2.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
